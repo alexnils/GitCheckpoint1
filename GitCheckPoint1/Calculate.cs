1107780d5aea50c1ee2740e7a64b291a72b912bb
alexnils <31803475+alexnils@users.noreply.github.com>
2023-02-16 14:41:56 +0100

    Add Calculate.cs and Add method

---
+++ GitCheckPoint1/Calculate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCheckpoint1
+{
+    internal class Calculate
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+    }
+}
